e4ddb759d2c8d9fe726077035e5c1a847b56f9fa
Sinan <abodsinan96@gmail.com>
2022-04-21 13:49:50 +0800

    Changed name of login endpoint

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,5 +7,5 @@ app_name = "authentication"
 
 url_patterns = [
     path("register", RegisterView.as_view(), name="register"),
-    path("api-token-auth", CustomAuthToken.as_view(), name="login" )
+    path("login", CustomAuthToken.as_view(), name="login" )
 ]
