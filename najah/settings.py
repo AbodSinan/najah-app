680213ca8e6cc311862b96481e0fa6f221bef05a
AbodSinan96 <abodsinan96@gmail.com>
2022-05-04 21:15:55 +0800

    Added image fields to profile

--- najah/settings.py
+++ najah/settings.py
@@ -100,7 +100,8 @@ DATABASES = {
     }
 }
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", None)
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_S3_ACCESS_KEY_ID", None)
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_S3_SECRET_ACCESS_KEY", None)
--- profile/models.py
+++ profile/models.py
@@ -18,6 +18,7 @@ class Profile(BaseModel):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     education_level = models.ForeignKey(EducationLevel, on_delete=models.SET_NULL, null=True)
     description = models.TextField(null=True)
+    image = models.ImageField(null=True)
 
     def __str__(self) -> str:
         return f"{self.user.email}:{self.user.first_name} ({self.user_type})"
