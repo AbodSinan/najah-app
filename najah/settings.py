80284b9c7a377972c90f8d722c0ee144537f32cf
AbodSinan96 <abodsinan96@gmail.com>
2023-01-22 18:59:10 +0800

    Fixed database settings (NEED TO FIND PERMANENT FIX)

--- najah/settings.py
+++ najah/settings.py
@@ -100,8 +100,12 @@ WSGI_APPLICATION = 'najah.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': "django.db.backends.postgresql",
+        "NAME": "najah",
+        "USER": "postgres",
+        "PASSWORD": "5WFz62MHVTXutdR",
+        "HOST": "najah-db.czpbbghrmffu.ap-southeast-1.rds.amazonaws.com",
+        "PORT": os.getenv("DB_PORT_AUTH", 5432),
     }
 }
 
