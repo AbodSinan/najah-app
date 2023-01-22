55744666778e3019292fc324d6ffee2e7b51e188
AbodSinan96 <abodsinan96@gmail.com>
2023-01-22 18:53:35 +0800

    Added CSRF exemption for github webhook view

--- najah/webhooks.py
+++ najah/webhooks.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 import subprocess
 
+@csrf_exempt
 def webhook(request):
     secret = request.headers.get('X-Hub-Signature')
     if secret != 'QWERTY123456':
