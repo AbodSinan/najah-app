a3edab61d74ecff10bb38d2d82ba38d7b261b3de
Abdullah Senan <abdullahsenan@Abdullahs-MacBook-Air.local>
2023-03-10 18:40:54 +0800

    Fixed education_level in profile serializer

--- profile/serializers.py
+++ profile/serializers.py
@@ -1,6 +1,14 @@
 from rest_framework import serializers
 
+from education.models import EducationLevel
 from profile.models import Profile
+class EducationLevelNameField(serializers.SerializerMethodField):
+    def to_representation(self, value):
+        try:
+            education_level = EducationLevel.objects.get(id=value.id)
+            return education_level.name
+        except EducationLevel.DoesNotExist:
+            return None
 
 class ProfileSerializer(serializers.ModelSerializer):
     first_name = serializers.CharField(required=False)
@@ -8,7 +16,6 @@ class ProfileSerializer(serializers.ModelSerializer):
     full_name = serializers.CharField(required=False)
     email = serializers.CharField(required=False)
     age = serializers.IntegerField(required=False)
-    education_level = serializers.CharField(source="education_level.name", required=False)
 
     class Meta:
         model = Profile
@@ -20,6 +27,13 @@ class ProfileSerializer(serializers.ModelSerializer):
         instance.user.last_name = validated_data.pop("last_name", instance.user.last_name)
         instance.user.email = validated_data.pop("email", instance.user.email)
         instance.user.save()
+        if "education_level" in validated_data:
+            try:
+                education_level = EducationLevel.objects.get(name=validated_data["education_level"])
+                instance.education_level = education_level
+                instance.save()
+            except EducationLevel.DoesNotExist:
+                raise serializers.ValidationError("Invalid education level")
         
         # Update the rest
         return super().update(instance, validated_data)
--- profile/tests/test_api.py
+++ profile/tests/test_api.py
@@ -1,11 +1,13 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
 
+from education.tests.factories import EducationLevelFactory
 from profile.tests.factories import ProfileFactory, UserFactory, TokenFactory
 
 class ProfileTestCase(APITestCase):
   OWN_PROFILE_URL = reverse("profile:profile")
   def setUp(self):
+    self.education_level = EducationLevelFactory(name="primary")
     self.user_1 = UserFactory(first_name="John Doe", email="JohnDoe@testing.com")
     self.user_2 = UserFactory(first_name="Jane Doe", email="JaneDoe@testing.com")
     self.profile_1 = ProfileFactory(age=15, user=self.user_1)
@@ -22,13 +24,14 @@ class ProfileTestCase(APITestCase):
     self.assertEqual(resp_data["first_name"], "John Doe")
 
   def test_update_profile_profile_fields(self):
-    req_data = {"age": 500, "description": "AM not gut"}
+    req_data = {"age": 500, "description": "AM not gut","education_level": self.education_level.id}
     resp = self.client.put(self.OWN_PROFILE_URL, data=req_data)
     self.assertEqual(resp.status_code, 200)
     self.profile_1.refresh_from_db()
 
     self.assertEqual(self.profile_1.age, 500)
     self.assertEqual(self.profile_1.description, "AM not gut")
+    self.assertEqual(self.profile_1.education_level, self.education_level)
 
   def test_update_profile_user_fields(self):
     req_data = {"last_name": "Doe Mama", "email": "doe@mama.com"}
